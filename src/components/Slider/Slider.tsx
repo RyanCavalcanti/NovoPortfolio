3cc6b13c8fd3751a4121eeec617ca3808bfb4dfd
Ryan Cavalcanti <ryancavalcanti192@gmail.com>
2023-11-29 15:19:35 -0300

    finalizando secao de slide e deixando-o responsivo.

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './Slider.module.scss';
 import { register } from 'swiper/element-bundle';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -12,24 +13,37 @@ import 'swiper/css/autoplay';
 const Slider = () => {
 
     const imagens = [
-        { id: '1',  image: '/src/assets/BannerResults.png', alt: 'Results Comunicação' },
-        { id: '2',  image: '/src/assets/BannerCeB.jpg', alt: 'CeB Consultoria' },
+        { id: '1',  image: '/src/assets/BannerResults.png', alt: 'Results Comunicação', href: 'https://www.resultscomunicacao.com/' },
+        { id: '2',  image: '/src/assets/BannerCeB.jpg', alt: 'CeB Consultoria', href: 'https://cebconsultoriadesegurancaocupacional.com/' },
     ]
 
+    const [indiceAtual, setIndiceAtual] = useState(0);
+
+    const handleSlideChange = (swiper: { realIndex: number }) => {
+        setIndiceAtual(swiper.realIndex);
+    };
 
   return (
     <section className={styles.container}>
     <Swiper
         modules={[Autoplay]}
         slidesPerView={1}
-        // autoplay={true}
+        onSlideChange={handleSlideChange}
+        //autoplay={true}
     >
         {imagens.map( (item) => (
             <SwiperSlide key={item.id} >
-                <img src={item.image} alt={item.alt}  className={styles.banner} />
+                <a href={item.href} target='_blank'><img src={item.image} alt={item.alt}  className={styles.banner} /></a>
             </SwiperSlide>
         ))}
     </Swiper>
+    
+    <div className={styles.indicadores}>
+                {imagens.map((_, index) => (
+                    <span key={index} className={index === indiceAtual ? styles.selecionado : styles.deselecionado}></span>
+                ))}
+            </div>
+           
     </section>
   );
 };
