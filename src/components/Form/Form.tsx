f00749535f8f2086ec73fee41f26122404c17c66
Ryan Cavalcanti <ryancavalcanti192@gmail.com>
2023-12-01 17:06:02 -0300

    adicionando php

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,10 +4,11 @@ const Form = () => {
     return(
         <section className={styles.container}>
                 <h3 className={styles.title}>Entre em contato</h3>
-                <form className={styles.form}>
+                <form className={styles.form} action='../../backend/form.php'>
+                    
                     <input className={styles.inputName} type="text" name="name" placeholder="Seu nome" required/>
                     <div className={styles.box}>
-                        <input className={styles.inputEmail} type="text" name="name" placeholder="Seu email" required/>
+                        <input className={styles.inputEmail} type="email" name="email" placeholder="Seu email" required/>
                         <input className={styles.inputNumber} type="text" name="number" placeholder="Seu WhatsApp" required/>
                     </div>
                     <button className={styles.btn}>Enviar</button>
