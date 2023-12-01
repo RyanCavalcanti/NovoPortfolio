0c69d38369f40b81ad4da458708ae8d5cb654bea
Ryan Cavalcanti <ryancavalcanti192@gmail.com>
2023-12-01 17:10:50 -0300

    adicionando metodo POST no form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,7 +4,7 @@ const Form = () => {
     return(
         <section className={styles.container}>
                 <h3 className={styles.title}>Entre em contato</h3>
-                <form className={styles.form} action='./form.php'>
+                <form className={styles.form} method='POST' action='./form.php'>
                     <input className={styles.inputName} type="text" name="name" placeholder="Seu nome" required/>
                     <div className={styles.box}>
                         <input className={styles.inputEmail} type="email" name="email" placeholder="Seu email" required/>
